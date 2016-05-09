1cb849d1c1cd3d5c3a1955612783bfd809048046
Allef Lobo <allef.lobo@gmail.com>
2016-05-09 13:00:20 -0300

    retira rota para doc

--- calculator_service/config/routes.rb
+++ calculator_service/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 	get "/" => "calculator#index"
 
 	resources :calculator, :only => [:index, :new, :create]
-	{Rails.root}/public/docs -> {Rails.root}/doc
-	
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
