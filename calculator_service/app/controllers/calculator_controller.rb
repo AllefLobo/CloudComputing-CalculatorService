734b0245ac687adcca4d2adc441dd9d718c12ac1
Allef Lobo <allef.lobo@gmail.com>
2016-05-15 01:40:35 -0300

    restricao do zero

--- calculator_service/app/controllers/calculator_controller.rb
+++ calculator_service/app/controllers/calculator_controller.rb
@@ -5,15 +5,28 @@ class CalculatorController < ApplicationController
     @secondValue = params["secondValue"].to_f
     @type = params["type"]
 
-    operation = Operation.new( params["type"], @firstValue, @secondValue)
-    @result = operation.calculate()
+		if @secondValue == 0.0
+			@result = 0.0
+		else
+    	operation = Operation.new( params["type"], @firstValue, @secondValue)
+    	@result = operation.calculate()
+		end
   end
 
   def create
-    operation = Operation.new( params[:type], params[:firstValue], params[:secondValue])
-    result = operation.calculate()
+		@firstValue = params["firstValue"].to_f
+    @secondValue = params["secondValue"].to_f
+    @type = params["type"]
+
+		if @secondValue == 0.0
+			result = 0.0
+		else
+			operation = Operation.new( @type, @firstValue, @secondValue)
+			result = operation.calculate()
+		end
+
     respond_to do |format|
-      msg = { :type => params[:type], :firstValue => params[:firstValue], :secondValue => params[:secondValue], :result => result }
+      msg = { :type => @type, :firstValue => @firstValue, :secondValue => @secondValue, :result => result }
       format.json  { render :json => msg }
     end
   end
